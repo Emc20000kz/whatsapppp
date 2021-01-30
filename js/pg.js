5f5f1ebb7eadaeaaa7b1a195289358e0a7869a1d
Emc20000kz <dafaprayoga2005@gmail.com>
2021-01-31 00:01:58 +0700

    First commit

---
+++ js/pg.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    particleground(document.getElementById('particles'), {
+        density: 7000,
+        dotColor: '#7489d8',
+        lineColor: '#7489d8',
+        particleRadius: 5,
+        parallax: false,
+
+    });
+    var intro = document.getElementById('intro');
+    intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
